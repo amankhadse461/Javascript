64e5c3c22693eda943c782d18b5897a0bd654348
aman <77562702+amankhadse461@users.noreply.github.com>
2023-02-25 11:15:55 +0530

    commit

---
+++ Chapter 6 Datatypes in JavaScript/datatpye.js
@@ -0,0 +1,30 @@
+// Datatype in Javascript
+
+//Number
+var a = 20;
+var b = 20;
+console.log(a,b);
+
+// string
+
+var firstname = "aman";
+var surname = "khadse";
+console.log(firstname+surname);
+
+//Boolen
+
+var a = true;
+var b = false;
+console.log(a,b);
+
+// Objects
+
+var marks = {
+   aman : 20,
+   ram : 10,
+   ravina :20
+};
+console.log(marks)
+
+var arr = ["mango","apple","pineapple"]
+console.log(arr)
