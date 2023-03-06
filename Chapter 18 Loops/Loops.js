6f4e5e46c6dafe0715cce925df3357a5c6c21fd7
aman <77562702+amankhadse461@users.noreply.github.com>
2023-03-06 15:59:40 +0530

    commit

---
+++ Chapter 18 Loops/Loops.js
@@ -0,0 +1,27 @@
+
+//1
+let a = 1
+for (let a = 1; a<100; a++)
+    {console.log(a)}
+    
+//2
+
+for (var i = 0; i < 100; i += 2 ) {
+console.log(i);
+}
+
+//continue
+for (var i = 0; i < 3; i++) {
+if (i === 1) {
+continue;
+}
+console.log(i);
+}
+
+//"while" Loops
+
+var i = 0;
+while (i < 100) {
+console.log(i);
+i++;
+}
