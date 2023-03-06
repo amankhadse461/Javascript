cbb23fcb2d4f521f8ef072ed4e1c270c55ae5b0e
aman <77562702+amankhadse461@users.noreply.github.com>
2023-03-06 15:26:57 +0530

    commit

---
+++ Chapter 10 Comparison Operations/Comparison Operations.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abstract equality / inequality and type conversion</title>
+</head>
+<body>
+    
+</body><script>
+    var a =10 
+    var b = 30
+    var result = a==b;
+    console.log(result)
+    var a =10 
+    var b = 30
+    var result = a>=b;
+    console.log(result)
+    var a =10 
+    var b = 30
+    var result = a<=b;
+    console.log(result)
+    var a =10 
+    var b = 30
+    var result = a!==b;
+    console.log(result)
+</script>
+</html>
