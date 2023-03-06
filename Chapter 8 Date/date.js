87d8714f741a38f24fd1e22f114a18bb5a47b4ef
aman <77562702+amankhadse461@users.noreply.github.com>
2023-03-06 15:22:54 +0530

    commit

---
+++ Chapter 8 Date/date.js
@@ -0,0 +1,30 @@
+// Date
+
+let myDate = new Date();
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
+
+//Convert to a string format
+
+//string
+var date1 = new Date();
+date1.toString();
+console.log(date1)
+
+// time
+var date1 = new Date();
+date1.toTimeString();
+console.log(date1)
+
+//Date String
+var date1 = new Date();
+date1.toDateString();
+console.log(date1)
+
+//Convert to JSON
+var date1 = new Date();
+date1.toJSON();
+console.log(date1)
